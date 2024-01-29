f24f2fcf4a58e263423905c80857c9e22e73d474
Jeremy Crisostomo <jeremycrisostomo@gmail.com>
2024-01-29 13:29:49 -0800

    updated currencies

--- curreny_calculations.py
+++ curreny_calculations.py
@@ -10,12 +10,13 @@ locale.setlocale(locale.LC_ALL, "")
 
 current_usdphp_rate = usd_php.get_usd_php_rate()
 
+
 def convert():
     while True:
         choice_convert = input(
             "\nHow would you like to convert?\n1. USD -> PHP\n2. PHP -> USD\nq) Go back\n\n: "
         )
-        if choice_convert == 'q':
+        if choice_convert == "q":
             return False
         elif choice_convert == "1":
             amount_convert = input("\nEnter the amount of USD to convert: $")
@@ -25,7 +26,7 @@ def convert():
             # return f"\n${amount_convert} converts to ₱{converted_value}.", True
             print(f"\n${amount_convert} converts to ₱{converted_value}.")
             continue
-        elif choice_convert == '2':
+        elif choice_convert == "2":
             amount_convert = input("\nEnter the amount of PHP to convert: ₱")
             choice_value = locale.atof(amount_convert)
             operation = choice_value / current_usdphp_rate
@@ -36,5 +37,5 @@ def convert():
 
 
 if __name__ == "__main__":
-    print(f'The current rate is $1 to ₱{current_usdphp_rate}')
+    print(f"The current rate is $1 to ₱{current_usdphp_rate}")
     convert()
